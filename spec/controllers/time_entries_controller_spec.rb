ae73cffe993581b188f3128e421965a072f69f0d
Joshua Plicque <plicjo@gmail.com>
2016-11-14 23:13:40 -0500

    Step 1: Discovery phase. Lay down a structure for understanding the controller action. Account for every conditional

--- spec/controllers/time_entries_controller_spec.rb
+++ spec/controllers/time_entries_controller_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 describe TimeEntriesController do
+  describe '#send_entries' do
+    it 'delivers an email'
+    it 'flashes a success message'
+    it 'redirects to root path'
+
+    context 'start date is present'
+      context 'end date is present'
+        it 'sends entries between start date and end date'
+
+      context 'end date is not present'
+        it 'sends entries between start date and today'
+
+    context 'start date is not present'
+      context 'end date is present'
+        it 'sends entries between today and end date'
+
+      context 'end date is not present'
+        it 'sends entries only from today'
+  end
 end
