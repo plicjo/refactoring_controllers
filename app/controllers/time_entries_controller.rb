17051f7ef671864c0da6027fe70028d42eb16db5
Joshua Plicque <plicjo@gmail.com>
2016-11-15 00:38:27 -0500

    Push query down into mailer. Problem solved

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -2,8 +2,7 @@ class TimeEntriesController < ApplicationController
   before_filter :authenticate_user!
 
   def send_entries
-    time_entries = TimeEntryQuery.new(start_date: params[:start_date], end_date: params[:end_date]).time_entries
-    UserMailer.send_time_entries(current_user, time_entries).deliver_now
+    UserMailer.send_time_entries(current_user, params).deliver_later
     flash[:success] = "Time Entries were successfully sent to you."
     redirect_to root_path
   end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
   default from: 'no-reply@example.com'
 
-  def send_time_entries(user, time_entries)
+  def send_time_entries(user, params)
     @recipient = user
-    @time_entries = time_entries
+    @time_entries = TimeEntryQuery.new(start_date: params[:start_date], end_date: params[:end_date]).time_entries
     mail(to: @user.email, subject: 'Your Time Entries Summary')
   end
 end
